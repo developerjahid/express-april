727b941943d4af0c522a0c64b64674a77e87e576
Developer Jahid <jhpabel@gmail.com>
2020-04-21 16:47:33 +0600

    nodemon

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World, nodemon installed'))
 
 app.listen(5000, () => console.log("Server running at port 5000!"))
